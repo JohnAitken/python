6567411b38a27412eafe72fceef4d16cf1fc3b18
JohnAitken <delgesu@gmail.com>
2023-10-22 14:08:38 +0100

    Popup version of ebook picker

--- ebook_picker.py
+++ ebook_picker.py
@@ -1,5 +1,8 @@
+import webbrowser
 import pandas as pd
 import random
+import tkinter as tk
+from tkinter import messagebox
 
 # Read the CSV file
 df = pd.read_csv('booklist_data.csv')
@@ -7,9 +10,25 @@ df = pd.read_csv('booklist_data.csv')
 # Randomly select a row based on 'File_Name'
 random_row = df.sample(n=1, random_state=42)
 
-# Get the 'File_Path' value from the randomly selected row
+# Get the 'File_Name' and 'File_Path' values from the randomly selected row
+file_name = random_row['File_Name'].values[0]
 file_path = random_row['File_Path'].values[0]
 
-# Print the randomized 'File_Name' and the corresponding 'File_Path' content
-print(f'Random File_Name: {random_row["File_Name"].values[0]}')
-print(f'File_Path Content: {file_path}')
+# Create a function to open a popup with a clickable hyperlink
+def open_popup():
+    popup = tk.Tk()
+    popup.title('Random ebook selector')
+    label = tk.Label(popup, text=f'Random File Name: {file_name}')
+    label.pack()
+    hyperlink = tk.Label(popup, text='Click here to open the file')
+    hyperlink.pack()
+    hyperlink.bind("<Button-1>", lambda event: open_file(file_path))
+    popup.mainloop()
+
+# Function to open the file when the hyperlink is clicked
+def open_file(file_path):
+    webbrowser.open(file_path)
+    # For example, you can use the webbrowser module to open URLs in the default web browser.
+
+# Display the popup with the clickable hyperlink
+open_popup()
