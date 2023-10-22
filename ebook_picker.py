db636c8a6100e99603f22565b117df6c9dcb64dc
JohnAitken <delgesu@gmail.com>
2023-10-22 14:21:09 +0100

    pop up picker done

--- ebook_picker.py
+++ ebook_picker.py
@@ -1,34 +1,54 @@
-import webbrowser
 import pandas as pd
 import random
 import tkinter as tk
-from tkinter import messagebox
+import webbrowser
 
 # Read the CSV file
 df = pd.read_csv('booklist_data.csv')
 
-# Randomly select a row based on 'File_Name'
-random_row = df.sample(n=1, random_state=42)
+# Initialize the random_row variable
+random_row = None
 
-# Get the 'File_Name' and 'File_Path' values from the randomly selected row
-file_name = random_row['File_Name'].values[0]
-file_path = random_row['File_Path'].values[0]
+# Function to randomize and select a row
+def randomize_and_select():
+    global random_row
+    random_row = df.sample(n=1, random_state=random.randint(0, 9999))
+    update_display()
 
-# Create a function to open a popup with a clickable hyperlink
-def open_popup():
-    popup = tk.Tk()
-    popup.title('Random ebook selector')
-    label = tk.Label(popup, text=f'Random File Name: {file_name}')
-    label.pack()
-    hyperlink = tk.Label(popup, text='Click here to open the file')
-    hyperlink.pack()
-    hyperlink.bind("<Button-1>", lambda event: open_file(file_path))
-    popup.mainloop()
+# Function to update the display with the new random row
+def update_display():
+    if random_row is not None:
+        file_name = random_row['File_Name'].values[0]
+        file_path = random_row['File_Path'].values[0]
+        label.config(text=f'Random File Name:       {file_name}')
+        hyperlink.config(text='Click here to open the file', cursor='hand2')
+        hyperlink.bind('<Button-1>', lambda event: open_file(file_path))
 
-# Function to open the file when the hyperlink is clicked
+# Function to open the URL in a web browser when clicked
 def open_file(file_path):
     webbrowser.open(file_path)
-    # For example, you can use the webbrowser module to open URLs in the default web browser.
 
-# Display the popup with the clickable hyperlink
-open_popup()
+# Create a GUI window
+root = tk.Tk()
+root.title('Random Ebook Picker')
+
+# Set a fixed width for the popup window
+root.geometry("600x100")  # Adjust the width as needed
+
+# Create a label for the text
+label = tk.Label(root, text='', padx=10, pady=10)
+label.pack()
+
+# Add a clickable hyperlink
+hyperlink = tk.Label(root, text='', fg='blue')
+hyperlink.pack()
+
+# Add a button to re-randomize
+randomize_button = tk.Button(root, text='Re-Randomize', command=randomize_and_select)
+randomize_button.pack()
+
+# Initial randomization
+randomize_and_select()
+
+# Start the main GUI loop
+root.mainloop()
