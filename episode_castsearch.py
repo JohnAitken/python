0bf36802e52ce1a11f06335c9a3b242e548600f6
JohnAitken <delgesu@gmail.com>
2023-11-06 21:29:03 +0000

    So much chatgpt - sorta works

--- episode_castsearch.py
+++ episode_castsearch.py
@@ -1,57 +1,81 @@
 import pandas as pd
-from tabulate import tabulate
 import tkinter as tk
-from tkinter import simpledialog
-from tkinter import Text
+from tkinter import Entry, Button, Label, Frame, Text, font
+from prettytable import PrettyTable
 
-def main():
-    dwguide = pd.read_csv("dwguide_clean.csv")
-    castlist = pd.read_csv("castlist.csv")
+result_frame = None
 
-    jd = pd.merge(dwguide, castlist, how="left", on="episodenbr")
+def display_results(data):
+    global result_frame
 
-    filter_value = simpledialog.askstring("Input", "Enter the value to filter by: ").lower()
+    if result_frame is not None:
+        result_frame.destroy()
 
+    result_frame = Frame(root)
+    result_frame.pack(fill='both', expand=True)
+
+    text_box = Text(result_frame, wrap=tk.WORD)
+    text_box.pack(fill='both', expand=True)
+    text_box.tag_configure("header", font=("Arial", 14, "bold"))
+    text_box.tag_configure("value", font=("Arial", 12))
+
+    parts = data.split('\n')
+    for part in parts:
+        if "Episode Details" in part:
+            text_box.insert(tk.END, part, "header")
+        elif "Episode Summary" in part:
+            text_box.insert(tk.END, part, "header")
+        elif "The Cast" in part:
+            text_box.insert(tk.END, part, "header")
+        else:
+            text_box.insert(tk.END, part, "value")
+        text_box.insert(tk.END, "\n")
+
+def filter_data():
+    filter_value = filter_input.get().lower()
     jd['title'] = jd['title'].str.lower()
     filtered_df = jd[jd['title'] == filter_value]
 
     if not filtered_df.empty:
-        # Create a pop-up dialog
-        popup = tk.Toplevel()
-        popup.title("Episode Details")
-
-        # Create an input field at the top
-        input_label = tk.Label(popup, text="Filter Value:")
-        input_label.pack()
-
-        input_text = tk.Entry(popup)
-        input_text.insert(0, filter_value)
-        input_text.config(state="readonly")
-        input_text.pack()
-
-        # Display Episode Details
         details_output = "Episode Details:\n"
-        filtered_df_1 = filtered_df.iloc[:1].drop(['role', 'name', 'summary'], axis=1)
-        details_output += tabulate(filtered_df_1, headers='keys', tablefmt='grid') + "\n"
+        details_df = filtered_df.iloc[:1].drop(['role', 'name', 'summary'], axis=1)
 
-        # Display Episode Summary
+        # Create a PrettyTable for the "Episode Details" table
+        details_table = PrettyTable()
+        details_table.field_names = details_df.columns
+        for _, row in details_df.iterrows():
+            details_table.add_row(row)
+
+        details_table.align = "l"  # Left-align all columns
+
+        details_output += details_table.get_string() + "\n"
+
         summary_output = "Episode Summary:\n"
-        filtered_df_2 = filtered_df[['summary']].head(1)
-        summary_output += tabulate(filtered_df_2, headers='keys', tablefmt='grid') + "\n"
+        summary_output += filtered_df['summary'].head(1).to_string(index=False) + "\n"
 
-        # Display The Cast
         cast_output = "The Cast:\n"
-        filtered_df_3 = filtered_df[['role', 'name']]
-        cast_output += tabulate(filtered_df_3, headers='keys', tablefmt='fancy_grid')
+        cast_output += filtered_df[['role', 'name']].to_string(index=False)
 
         combined_output = details_output + summary_output + cast_output
-
-        # Create a text box to display the results below the input field
-        text_box = Text(popup, height=20, width=80)
-        text_box.insert(tk.END, combined_output)
-        text_box.pack()
+        display_results(combined_output)
     else:
-        tk.messagebox.showinfo("Info", f"No rows found with title equal to {filter_value}")
+        display_results(f"No rows found with title equal to {filter_value}")
 
-if __name__ == "__main__":
-    main()
+root = tk.Tk()
+root.title("Search and Display")
+root.geometry("800x600")
+
+dwguide = pd.read_csv("dwguide_clean.csv")
+castlist = pd.read_csv("castlist.csv")
+jd = pd.merge(dwguide, castlist, how="left", on="episodenbr")
+
+filter_input_label = Label(root, text="Enter the value to filter by:")
+filter_input_label.pack()
+
+filter_input = Entry(root)
+filter_input.pack()
+
+search_button = Button(root, text="Search", command=filter_data)
+search_button.pack()
+
+root.mainloop()
