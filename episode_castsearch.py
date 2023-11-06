b7b1f2cc26c73f01fc824d46e93e2e7391f755a0
JohnAitken <delgesu@gmail.com>
2023-11-06 20:36:08 +0000

    first draft cgpt - does not work.

--- episode_castsearch.py
+++ episode_castsearch.py
@@ -1,34 +1,57 @@
 import pandas as pd
 from tabulate import tabulate
+import tkinter as tk
+from tkinter import simpledialog
+from tkinter import Text
 
+def main():
+    dwguide = pd.read_csv("dwguide_clean.csv")
+    castlist = pd.read_csv("castlist.csv")
 
-dwguide = pd.read_csv("dwguide_clean.csv")
-castlist = pd.read_csv("castlist.csv")
+    jd = pd.merge(dwguide, castlist, how="left", on="episodenbr")
 
-jd = pd.merge(dwguide, castlist, how="left", on="episodenbr")
+    filter_value = simpledialog.askstring("Input", "Enter the value to filter by: ").lower()
 
-# Ask the user for the value to filter by (and convert it to lowercase)
-filter_value = input("Enter the value to filter by: ").lower()
+    jd['title'] = jd['title'].str.lower()
+    filtered_df = jd[jd['title'] == filter_value]
 
-# Convert the values in the 'title' column to lowercase for case-insensitive comparison
-jd['title'] = jd['title'].str.lower()
+    if not filtered_df.empty:
+        # Create a pop-up dialog
+        popup = tk.Toplevel()
+        popup.title("Episode Details")
 
-# Filter the DataFrame while ignoring case differences
-filtered_df = jd[jd['title'] == filter_value]
+        # Create an input field at the top
+        input_label = tk.Label(popup, text="Filter Value:")
+        input_label.pack()
 
-# Display the first row of the filtered DataFrame (excluding 'role' and 'name' columns)
-if not filtered_df.empty:
-    print("Episode Details")
-    filtered_df_1 = filtered_df.iloc[:1].drop(['role', 'name', 'summary'], axis=1)
-    print(tabulate(filtered_df_1, headers='keys', tablefmt='grid'))
-    
-    print("\nEpisode summary")
-    filtered_df_1 = filtered_df[['summary']].head(1)
-    print(tabulate(filtered_df_1, headers='keys', tablefmt='grid'))
+        input_text = tk.Entry(popup)
+        input_text.insert(0, filter_value)
+        input_text.config(state="readonly")
+        input_text.pack()
 
-    # Display the 'name' and 'role' columns for the entire filtered DataFrame
-    print("\nThe Cast:")
-    filtered_df_2 = filtered_df[['role', 'name']]
-    print(tabulate(filtered_df_2, headers='keys', tablefmt='fancy_grid'))
-else:
-    print(f"No rows found with title equal to {filter_value}")
+        # Display Episode Details
+        details_output = "Episode Details:\n"
+        filtered_df_1 = filtered_df.iloc[:1].drop(['role', 'name', 'summary'], axis=1)
+        details_output += tabulate(filtered_df_1, headers='keys', tablefmt='grid') + "\n"
+
+        # Display Episode Summary
+        summary_output = "Episode Summary:\n"
+        filtered_df_2 = filtered_df[['summary']].head(1)
+        summary_output += tabulate(filtered_df_2, headers='keys', tablefmt='grid') + "\n"
+
+        # Display The Cast
+        cast_output = "The Cast:\n"
+        filtered_df_3 = filtered_df[['role', 'name']]
+        cast_output += tabulate(filtered_df_3, headers='keys', tablefmt='fancy_grid')
+
+        combined_output = details_output + summary_output + cast_output
+
+        # Create a text box to display the results below the input field
+        text_box = Text(popup, height=20, width=80)
+        text_box.insert(tk.END, combined_output)
+        text_box.pack()
+    else:
+        tk.messagebox.showinfo("Info", f"No rows found with title equal to {filter_value}")
+
+if __name__ == "__main__":
+    main()
