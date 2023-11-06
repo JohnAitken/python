e39e76f518f6afe178d4f341253725e1ad115ee0
JohnAitken <delgesu@gmail.com>
2023-11-06 21:39:49 +0000

    FLask used for browser server testing

--- episode_castsearch.py
+++ episode_castsearch.py
@@ -1,81 +1,43 @@
 import pandas as pd
-import tkinter as tk
-from tkinter import Entry, Button, Label, Frame, Text, font
-from prettytable import PrettyTable
+from flask import Flask, request, render_template_string
 
-result_frame = None
+app = Flask(__name__)
 
-def display_results(data):
-    global result_frame
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        filter_value = request.form['filter_value'].lower()
+        jd['title'] = jd['title'].str.lower()
+        filtered_df = jd[jd['title'] == filter_value]
 
-    if result_frame is not None:
-        result_frame.destroy()
+        if not filtered_df.empty:
+            details_output = "<h1>Episode Details:</h1>"
+            details_df = filtered_df.iloc[:1].drop(['role', 'name', 'summary'], axis=1)
+            details_output += details_df.to_html(index=False)
 
-    result_frame = Frame(root)
-    result_frame.pack(fill='both', expand=True)
+            summary_output = "<h1>Episode Summary:</h1>"
+            summary_output += filtered_df['summary'].head(1).to_string(index=False)
 
-    text_box = Text(result_frame, wrap=tk.WORD)
-    text_box.pack(fill='both', expand=True)
-    text_box.tag_configure("header", font=("Arial", 14, "bold"))
-    text_box.tag_configure("value", font=("Arial", 12))
+            cast_output = "<h1>The Cast:</h1>"
+            cast_output += filtered_df[['role', 'name']].to_html(index=False)
 
-    parts = data.split('\n')
-    for part in parts:
-        if "Episode Details" in part:
-            text_box.insert(tk.END, part, "header")
-        elif "Episode Summary" in part:
-            text_box.insert(tk.END, part, "header")
-        elif "The Cast" in part:
-            text_box.insert(tk.END, part, "header")
+            combined_output = details_output + summary_output + cast_output
         else:
-            text_box.insert(tk.END, part, "value")
-        text_box.insert(tk.END, "\n")
+            combined_output = f"No rows found with title equal to {filter_value}"
 
-def filter_data():
-    filter_value = filter_input.get().lower()
-    jd['title'] = jd['title'].str.lower()
-    filtered_df = jd[jd['title'] == filter_value]
+        return render_template_string(combined_output)
 
-    if not filtered_df.empty:
-        details_output = "Episode Details:\n"
-        details_df = filtered_df.iloc[:1].drop(['role', 'name', 'summary'], axis=1)
+    return """
+    <form method="POST">
+        <label for="filter_value">Enter the value to filter by:</label>
+        <input type="text" name="filter_value">
+        <input type="submit" value="Search">
+    </form>
+    """
 
-        # Create a PrettyTable for the "Episode Details" table
-        details_table = PrettyTable()
-        details_table.field_names = details_df.columns
-        for _, row in details_df.iterrows():
-            details_table.add_row(row)
-
-        details_table.align = "l"  # Left-align all columns
-
-        details_output += details_table.get_string() + "\n"
-
-        summary_output = "Episode Summary:\n"
-        summary_output += filtered_df['summary'].head(1).to_string(index=False) + "\n"
-
-        cast_output = "The Cast:\n"
-        cast_output += filtered_df[['role', 'name']].to_string(index=False)
-
-        combined_output = details_output + summary_output + cast_output
-        display_results(combined_output)
-    else:
-        display_results(f"No rows found with title equal to {filter_value}")
-
-root = tk.Tk()
-root.title("Search and Display")
-root.geometry("800x600")
-
-dwguide = pd.read_csv("dwguide_clean.csv")
-castlist = pd.read_csv("castlist.csv")
-jd = pd.merge(dwguide, castlist, how="left", on="episodenbr")
-
-filter_input_label = Label(root, text="Enter the value to filter by:")
-filter_input_label.pack()
-
-filter_input = Entry(root)
-filter_input.pack()
-
-search_button = Button(root, text="Search", command=filter_data)
-search_button.pack()
-
-root.mainloop()
+if __name__ == '__main__':
+    dwguide = pd.read_csv("dwguide_clean.csv")
+    castlist = pd.read_csv("castlist.csv")
+    jd = pd.merge(dwguide, castlist, how="left", on="episodenbr")
+    
+    app.run(debug=True)
